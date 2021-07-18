8bb398ce509d463288badee02f729e97c1061705
Udit Patel <admin@uditpatel.com>
2021-07-18 07:04:11 +0530

    added v2 v3 contact

---
+++ partials/body/contact/_v3-in_contact-in_body.php
@@ -0,0 +1,43 @@
+<!-- Version3 Contact Start here-->
+
+<!-- Contact Section Heading -->
+        <h2 class="page-section-heading display-4 mb-4"><strong>Contact Me</strong></h2>
+
+        <!-- Contact Form Container -->
+        <div class="row justify-content-center text-center">
+          <div class="col-sm-12 col-md-12 col-lg-12">
+            <!-- Contact Form -->
+            <form class="border border-secondary rounded px-4 pt-5 pb-sm-3">
+
+              <div class="row">
+                <!-- Form Col -->
+                <div class="order-1 col-sm-12 col-md-6 col-lg-6">
+                  <div class="form-group pb-1">
+                    <input type="text" class="form-control form-control-lg" id="name" placeholder="Name:">
+                  </div>
+                  <div class="form-group pb-1">
+                    <input type="text" class="form-control form-control-lg" id="email" placeholder="Email:">
+                  </div>
+                  <div class="form-group pb-1">
+                    <textarea class="form-control form-control-lg" id="message" placeholder="Message:" rows="3"></textarea>
+                  </div>
+                  <div class="my-3">
+                    <button type="submit" class="btn btn-secondary btn-lg">Submit</button>
+                  </div>
+                </div>
+                <!-- Form Col -->
+
+                <!-- Form Image Col -->
+                <div class="order-2 col-sm-12 col-md-6 col-lg-6 pb-2">
+                  <img src="./partials/img/image-1.png" class="rounded m-auto" style="width: 100%" alt="Form Image description">
+                </div>
+                <!-- /Form Image Col -->
+              </div>
+
+            </form>
+            <!-- / Contact Form -->
+          </div>
+        </div>
+        <!-- /Contact Form Container-->
+
+<!-- /Version3 Contact End here-->
