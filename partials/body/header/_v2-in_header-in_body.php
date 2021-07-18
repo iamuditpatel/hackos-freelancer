12c0d5fa5e8daa3aade14f2fe2444a26a2d0eb1e
Udit Patel <admin@uditpatel.com>
2021-07-18 06:50:38 +0530

    added v2 v3 header

---
+++ partials/body/header/_v2-in_header-in_body.php
@@ -0,0 +1,11 @@
+<!-- Vesion2 Header Start here -->
+<header class="masthead bg-primary text-white text-center mt-5 vh-100">
+        <div class="container pt-5">
+          <!-- Masthead Avatar Image --><img src="./partials/img/profile-img .png" width="300" height="300" class="masthead-avatar rounded border border-dark mt-5 mb-3" alt="freelancer profile photo">
+          <!-- Masthead Heading-->
+          <h1 class="masthead-heading mb-0">Hi I am Name</h1>
+          <!-- Masthead Subheading-->
+          <p class="masthead-subheading font-weight-light">Web Developer | Graphic Designer</p>
+        </div>
+    </header>
+<!-- /Vesion3 Header Start here -->
---
+++ partials/body/header/_v3-in_header-in_body.php
@@ -0,0 +1,20 @@
+<!-- Vesion3 Header Start here -->
+<header class="masthead bg-primary text-white text-center mt-5 vh-100">
+        <div class="container pt-5 d-flex align-items-center justify-content-center">
+          <div class="row">
+            <div class="col-sm-12 col-md-6 col-lg-6 pt-5 my-auto">
+              <!-- Masthead Heading-->
+              <h1 class="masthead-heading mb-0">Hi I am Name</h1>
+              <!-- Masthead Subheading-->
+              <!-- <p class="masthead-subheading font-weight-light">Web Developer | Graphic Designer</p> -->
+              <span class="badge badge-pill badge-light">Web Developer</span>
+              <span class="badge badge-pill badge-light">Graphic Designer</span>
+            </div>
+            <div class="col col-sm-12 col-md-6 col-lg-6 pt-5">
+              <!-- Masthead Avatar Image -->
+              <img src="./partials/img/profile-img .png" width="300" height="300" class="masthead-avatar rounded border border-dark" alt="freelancer profile photo">
+            </div>
+          </div>
+        </div>
+    </header>
+<!-- /Vesion3 Header Start here -->
