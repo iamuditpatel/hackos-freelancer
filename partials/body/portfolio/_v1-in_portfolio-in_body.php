a94f22808901ede03bc1c88b10c80c30580ddeb0
Udit Patel <uditatulpatel@gmail.com>
2021-07-18 06:53:24 +0530

    added v1 portfolio

---
+++ partials/body/portfolio/_v1-in_portfolio-in_body.php
@@ -0,0 +1,60 @@
+<!-- Version1 Portfolio Start here-->
+
+<!-- Portfolio Section Heading-->
+<h2 class="page-section-heading display-4 text-center text-secondary mb-3"><strong>Portfolio</strong></h2>
+
+<!-- Portfolio Grid Items-->
+<div class="row row-cols-1 row-cols-md-2">
+
+  <!-- Portfolio Item-->
+  <div class="col-12 col-md-6 col-lg-4 py-3">
+    <div class="card">
+      <img src="./partials/img/image-1.png" class="card-img-top" style="width: 100%;" alt="Portfolio item description">
+    </div>
+  </div>
+  <!-- /Portfolio Item-->
+
+  <!-- Portfolio Item-->
+  <div class="col-12 col-md-6 col-lg-4 py-3">
+    <div class="card">
+      <img src="./partials/img/image-2.png" class="card-img-top" style="width: 100%;" alt="Portfolio item description">
+    </div>
+  </div>
+  <!-- /Portfolio Item-->
+
+  <!-- Portfolio Item-->
+  <div class="col-12 col-md-6 col-lg-4 py-3">
+    <div class="card">
+      <img src="./partials/img/image-3.png" class="card-img-top" style="width: 100%;" alt="Portfolio item description">
+    </div>
+  </div>
+  <!-- /Portfolio Item-->
+
+  <!-- Portfolio Item-->
+  <div class="col-12 col-md-6 col-lg-4 py-3">
+    <div class="card">
+      <img src="./partials/img/image-4.png" class="card-img-top" style="width: 100%;" alt="Portfolio item description">
+    </div>
+  </div>
+  <!-- /Portfolio Item-->
+
+  <!-- Portfolio Item-->
+  <div class="col-12 col-md-6 col-lg-4 py-3">
+    <div class="card">
+      <img src="./partials/img/image-5.png" class="card-img-top" style="width: 100%;" alt="Portfolio item description">
+    </div>
+  </div>
+  <!-- /Portfolio Item-->
+
+  <!-- Portfolio Item-->
+  <div class="col-12 col-md-6 col-lg-4 py-3">
+    <div class="card">
+      <img src="./partials/img/image-6.png" class="card-img-top" style="width: 100%;" alt="Portfolio item description">
+    </div>
+  </div>
+  <!-- /Portfolio Item-->
+
+</div>
+<!-- /Portfolio Grid Items-->
+
+<!-- /Version1 Portfolio End here-->
