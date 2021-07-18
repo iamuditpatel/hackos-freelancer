83d7ef324fbb74e0c3041fce2117d202180078a4
Udit Patel <uditatulpatel@gmail.com>
2021-07-18 06:31:29 +0530

    added css in head

---
+++ partials/head/css/_css-in_head.php
@@ -0,0 +1,7 @@
+<!-- Bootstrap CSS Start here -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<!-- /Bootstrap CSS End here -->
+
+<!-- Custom CSS Start here -->
+<link rel="stylesheet" href="./lib/css/style.css">
+<!-- /Custom CSS End here -->
