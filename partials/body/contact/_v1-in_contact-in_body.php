f990d364bedcc0aa23b5a48cfc7ee4a10ae9b6f3
Udit Patel <uditatulpatel@gmail.com>
2021-07-18 07:03:41 +0530

    added v1 contact

---
+++ partials/body/contact/_v1-in_contact-in_body.php
@@ -0,0 +1,27 @@
+<!-- Version1 Contact Start here-->
+
+<!-- Contact Section Heading -->
+<h2 class="page-section-heading display-4 mb-4"><strong>Contact Me</strong></h2>
+
+<!-- Contact Form Container -->
+<div class="row justify-content-center text-center">
+  <div class="col-sm-12 col-md-12 col-lg-6">
+    <!-- Contact Form -->
+    <form class="border border-secondary rounded px-4 py-5">
+      <div class="form-group pb-1">
+        <input type="text" class="form-control form-control-lg" id="name" placeholder="Name:">
+      </div>
+      <div class="form-group pb-1">
+        <input type="text" class="form-control form-control-lg" id="email" placeholder="Email:">
+      </div>
+      <div class="form-group pb-1">
+        <textarea class="form-control form-control-lg" id="message" placeholder="Message:" rows="3"></textarea>
+      </div>
+      <button type="submit" class="btn btn-secondary btn-lg mt-3">Submit</button>
+    </form>
+    <!-- / Contact Form -->
+  </div>
+</div>
+<!-- /Contact Form Container-->
+
+<!-- /Version1 Contact End here-->
