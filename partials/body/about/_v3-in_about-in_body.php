d0fba430b9b0e95c702d439c38eda54894e6becc
Udit Patel <admin@uditpatel.com>
2021-07-18 06:56:03 +0530

    added v2 v3 about

---
+++ partials/body/about/_v3-in_about-in_body.php
@@ -0,0 +1,27 @@
+<!-- Version3 About Start here-->
+
+<!-- About Section Heading -->
+        <h2 class="page-section-heading display-4 pt-5 mb-3"><strong>About</strong></h2>
+
+        <!--  About Info -->
+        <div class="row row-cols-1 row-cols-md-2 justify-content-center text-justify">
+
+          <!-- About Text -->
+          <div class="col-12 col-md-12 col-lg-6 py-3">
+            <p><strong>Your about info goes here</strong></p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vulputate sem nisi, eget faucibus diam ullamcorper vitae. Donec ac sapien tellus. Praesent venenatis metus vitae nulla malesuada egestas. Curabitur feugiat, ligula in dictum tempus, risus dui bibendum odio, quis tincidunt mauris nulla et magna. Nam iaculis nulla nec nunc volutpat, nec viverra justo varius. Fusce vulputate in mauris at placerat. Fusce eros sem, ullamcorper id quam ac, vehicula iaculis arcu. Phasellus molestie ex vitae felis porta, ac dapibus lectus dignissim. Sed porttitor ornare magna tristique aliquet. Mauris hendrerit, sapien et feugiat feugiat, risus orci consectetur risus, at aliquam ipsum metus ac eros. Mauris eget lorem at lorem varius volutpat quis quis nisi. In laoreet, neque a mollis tempor, quam tellus commodo erat, id sagittis ipsum nisl vitae dui. Aliquam consequat magna vel arcu condimentum laoreet. Maecenas aliquet viverra tortor eget tempus. Morbi feugiat, sapien quis pharetra rutrum, libero turpis fringilla dui, ut vehicula nulla metus at lacus.
+            </p>
+          </div>
+          <!--  /About Text -->
+
+          <!-- About Image -->
+          <div class="col-12 col-md-8 col-lg-6 py-3">
+            <img src="./partials/img/image-1.png" class="rounded m-auto d-block" style="width: 95%" alt="About Image description">
+          </div>
+          <!--  /About Image -->
+
+        </div>
+        <!--  /About Info -->
+
+<!-- /Version3 About Start here-->
