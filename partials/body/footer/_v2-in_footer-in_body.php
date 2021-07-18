3fafeb6b332a7e7cf468ffac852639622d6dc92c
Udit Patel <admin@uditpatel.com>
2021-07-18 07:06:17 +0530

    added v2 v3 footer

---
+++ partials/body/footer/_v2-in_footer-in_body.php
@@ -0,0 +1,37 @@
+<!-- Version2 Footer Start here-->
+
+<div class="row py-5">
+          <!-- Footer Social Icons-->
+          <div class="col-lg-4 mb-5 mt-4 mb-lg-0">
+            <h4 class="mb-4">THE WEB</h4>
+            <a class="btn btn-outline-light btn-social mx-1" href="#">
+              <i class="fab fa-fw fa-facebook-f"></i>
+            </a>
+            <a class="btn btn-outline-light btn-social mx-1" href="#">
+              <i class="fab fa-fw fa-twitter"></i>
+            </a>
+            <a class="btn btn-outline-light btn-social mx-1" href="#">
+              <i class="fab fa-fw fa-linkedin-in"></i>
+            </a>
+            <a class="btn btn-outline-light btn-social mx-1" href="#">
+              <i class="fab fa-fw fa-instagram"></i>
+            </a>
+          </div>
+          <!-- /Footer Social Icons-->
+
+          <!-- Footer Location-->
+          <div class="col-lg-4 mb-5 mt-4 mb-lg-0">
+            <h4 class="mb-4">LOCATION</h4>
+            <p class="pre-wrap lead mb-0">912  Patton Lane, North Carolina</p>
+          </div>
+          <!-- /Footer Location-->
+
+          <!-- Footer About Text-->
+          <div class="col-lg-4 mb-3 mt-4">
+            <h4 class="mb-4">ABOUT FREELANCER</h4>
+            <p class="pre-wrap lead mb-0">Free to use, MIT licensed Bootstrap theme created by Tropos</p>
+          </div>
+          <!-- /Footer About Text-->
+        </div>
+
+<!-- /Version2 Footer End here-->
---
+++ partials/body/footer/_v3-in_footer-in_body.php
@@ -0,0 +1,35 @@
+<!-- Version3 Footer Start here-->
+
+<div class="row py-5">
+          <!-- Footer Copyright-->
+          <div class="col-lg-4 mb-5 mt-4 mb-lg-0">
+            <p class="pre-wrap lead mb-0">Copyright &copy; example.com</p>
+          </div>
+          <!-- /Footer Copyright-->
+
+          <!-- Footer Social Icons-->
+          <div class="col-lg-4 mb-5 mt-4 mb-lg-0">
+            <a class="btn btn-outline-light btn-social mx-1" href="#">
+              <i class="fab fa-fw fa-facebook-f"></i>
+            </a>
+            <a class="btn btn-outline-light btn-social mx-1" href="#">
+              <i class="fab fa-fw fa-twitter"></i>
+            </a>
+            <a class="btn btn-outline-light btn-social mx-1" href="#">
+              <i class="fab fa-fw fa-linkedin-in"></i>
+            </a>
+            <a class="btn btn-outline-light btn-social mx-1" href="#">
+              <i class="fab fa-fw fa-instagram"></i>
+            </a>
+          </div>
+          <!-- /Footer Social Icons-->
+
+          <!-- Footer About Text-->
+          <div class="col-lg-4 mb-3 mt-4">
+            <h4 class="mb-4">ABOUT FREELANCER</h4>
+            <p class="pre-wrap lead mb-0">Free to use, MIT licensed Bootstrap theme created by Tropos</p>
+          </div>
+          <!-- /Footer About Text-->
+        </div>
+
+<!-- /Version3 Footer End here-->
