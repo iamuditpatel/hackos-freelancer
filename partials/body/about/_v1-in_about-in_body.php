b071600771096f22beae30a16999eaedab57eb49
Udit Patel <uditatulpatel@gmail.com>
2021-07-18 06:55:28 +0530

    added v1 about

---
+++ partials/body/about/_v1-in_about-in_body.php
@@ -0,0 +1,30 @@
+<!-- Version1 About Start here-->
+
+<!-- About Section Heading -->
+        <h2 class="page-section-heading display-4 pt-5 mb-3"><strong>About</strong></h2>
+
+        <!--  About Info-->
+        <div class="row row-cols-1 row-cols-md-2 justify-content-center text-justify">
+
+          <!-- About Col-1-->
+          <div class="col-12 col-md-6 col-lg-6 order-1 py-3">
+            <p><strong>Your about info goes here</strong></p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vulputate sem nisi, eget faucibus diam ullamcorper vitae. Donec ac sapien tellus. Praesent venenatis metus vitae nulla malesuada egestas. Curabitur feugiat, ligula in dictum tempus, risus dui bibendum odio, quis tincidunt mauris nulla et magna. Nam iaculis nulla nec nunc volutpat, nec viverra justo varius. Fusce vulputate in mauris at placerat. Fusce eros sem, ullamcorper id quam ac, vehicula iaculis arcu. Phasellus molestie ex vitae felis porta, ac dapibus lectus dignissim. Sed porttitor ornare magna tristique aliquet. Mauris hendrerit, sapien et feugiat feugiat, risus orci consectetur risus, at aliquam ipsum metus ac eros. Mauris eget lorem at lorem varius volutpat quis quis nisi. In laoreet, neque a mollis tempor, quam tellus commodo erat, id sagittis ipsum nisl vitae dui. Aliquam consequat magna vel arcu condimentum laoreet. Maecenas aliquet viverra tortor eget tempus. Morbi feugiat, sapien quis pharetra rutrum, libero turpis fringilla dui, ut vehicula nulla metus at lacus.
+            </p>
+          </div>
+          <!--  /About Col-1-->
+
+          <!-- About Col-2-->
+          <div class="col-12 col-md-6 col-lg-6 order-2 py-3">
+            <p><strong>Your about info goes here</strong></p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vulputate sem nisi, eget faucibus diam ullamcorper vitae. Donec ac sapien tellus. Praesent venenatis metus vitae nulla malesuada egestas. Curabitur feugiat, ligula in dictum tempus, risus dui bibendum odio, quis tincidunt mauris nulla et magna. Nam iaculis nulla nec nunc volutpat, nec viverra justo varius. Fusce vulputate in mauris at placerat. Fusce eros sem, ullamcorper id quam ac, vehicula iaculis arcu. Phasellus molestie ex vitae felis porta, ac dapibus lectus dignissim. Sed porttitor ornare magna tristique aliquet. Mauris hendrerit, sapien et feugiat feugiat, risus orci consectetur risus, at aliquam ipsum metus ac eros. Mauris eget lorem at lorem varius volutpat quis quis nisi. In laoreet, neque a mollis tempor, quam tellus commodo erat, id sagittis ipsum nisl vitae dui. Aliquam consequat magna vel arcu condimentum laoreet. Maecenas aliquet viverra tortor eget tempus. Morbi feugiat, sapien quis pharetra rutrum, libero turpis fringilla dui, ut vehicula nulla metus at lacus.
+            </p>
+          </div>
+          <!--  /About Col-2-->
+
+        </div>
+        <!--  /About Info-->
+
+<!-- /Version1 About Start here-->
