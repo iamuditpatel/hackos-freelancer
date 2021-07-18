4fa9930ccd9fb1ed39f912372b7907c7ce7c46a8
Udit Patel <admin@uditpatel.com>
2021-07-18 06:54:12 +0530

    added v2 v3 portfolio

---
+++ partials/body/portfolio/_v2-in_portfolio-in_body.php
@@ -0,0 +1,65 @@
+<!-- Version2 Portfolio Start here-->
+
+<!-- Portfolio Section Heading-->
+        <h2 class="page-section-heading display-4 text-center text-secondary mb-3"><strong>Portfolio</strong></h2>
+        <!-- Portfolio Grid Items-->
+        <div class="row row-cols-1 row-cols-md-2">
+
+          <!-- Portfolio Item-->
+          <div class="col-12 col-md-6 col-lg-4 py-3">
+            <div>
+              <img src="./partials/img/image-1.png" class="rounded-circle mx-auto d-block" width="250" height="250" alt="Portfolio item description">
+            </div>
+            <h3 class="text-center mt-2">example.com</h3>
+          </div>
+          <!-- /Portfolio Item-->
+
+          <!-- Portfolio Item-->
+          <div class="col-12 col-md-6 col-lg-4 py-3">
+            <div>
+              <img src="./partials/img/image-2.png" class="rounded-circle mx-auto d-block" width="250" height="250" alt="Portfolio item description">
+            </div>
+            <h3 class="text-center mt-2">example.com</h3>
+          </div>
+          <!-- /Portfolio Item-->
+
+          <!-- Portfolio Item-->
+          <div class="col-12 col-md-6 col-lg-4 py-3">
+            <div>
+              <img src="./partials/img/image-3.png" class="rounded-circle mx-auto d-block" width="250" height="250" alt="Portfolio item description">
+            </div>
+            <h3 class="text-center mt-2">example.com</h3>
+          </div>
+          <!-- /Portfolio Item-->
+
+          <!-- Portfolio Item-->
+          <div class="col-12 col-md-6 col-lg-4 py-3">
+            <div>
+              <img src="./partials/img/image-4.png" class="rounded-circle mx-auto d-block" width="250" height="250" alt="Portfolio item description">
+            </div>
+            <h3 class="text-center mt-2">example.com</h3>
+          </div>
+          <!-- /Portfolio Item-->
+
+          <!-- Portfolio Item-->
+          <div class="col-12 col-md-6 col-lg-4 py-3">
+            <div>
+              <img src="./partials/img/image-5.png" class="rounded-circle mx-auto d-block" width="250" height="250" alt="Portfolio item description">
+            </div>
+            <h3 class="text-center mt-2">example.com</h3>
+          </div>
+          <!-- /Portfolio Item-->
+
+          <!-- Portfolio Item-->
+          <div class="col-12 col-md-6 col-lg-4 py-3">
+            <div>
+              <img src="./partials/img/image-6.png" class="rounded-circle mx-auto d-block" width="250" height="250" alt="Portfolio item description">
+            </div>
+            <h3 class="text-center mt-2">example.com</h3>
+          </div>
+          <!-- /Portfolio Item-->
+
+        </div>
+        <!-- /Portfolio Grid Items-->
+
+  <!-- /Version2 Portfolio End here-->
---
+++ partials/body/portfolio/_v3-in_portfolio-in_body.php
@@ -0,0 +1,119 @@
+<!-- Version3 Portfolio Start here-->
+
+<!-- Portfolio Section Heading-->
+        <h2 class="page-section-heading display-4 text-center text-secondary mb-3"><strong>Portfolio</strong></h2>
+        <!-- Portfolio Grid Items-->
+        <div class="row">
+
+          <!-- Portfolio Item-->
+          <div class="col-12 col-md-6 col-lg-6 py-3">
+            <div class="card mb-3" style="max-width: 540px;">
+              <div class="row no-gutters">
+                <div class="col-md-12 col-lg-7">
+                  <img src="./partials/img/image-1.png" class="rounded mx-auto d-block" width="100%" height="100%" alt="Portfolio item description">
+                </div>
+                <div class="col-md-12 col-lg-5">
+                  <div class="card-body">
+                    <h5 class="card-title text-center">example.com</h5>
+                    <p class="card-text">This section is for project description.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- /Portfolio Item-->
+
+          <!-- Portfolio Item-->
+          <div class="col-12 col-md-6 col-lg-6 py-3">
+            <div class="card mb-3" style="max-width: 540px;">
+              <div class="row no-gutters">
+                <div class="col-md-12 col-lg-7">
+                  <img src="./partials/img/image-2.png" class="rounded mx-auto d-block" width="100%" height="100%" alt="Portfolio item description">
+                </div>
+                <div class="col-md-12 col-lg-5">
+                  <div class="card-body">
+                    <h5 class="card-title text-center">example.com</h5>
+                    <p class="card-text">This section is for project description.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- /Portfolio Item-->
+
+          <!-- Portfolio Item-->
+          <div class="col-12 col-md-6 col-lg-6 py-3">
+            <div class="card mb-3" style="max-width: 540px;">
+              <div class="row no-gutters">
+                <div class="col-md-12 col-lg-7">
+                  <img src="./partials/img/image-3.png" class="rounded mx-auto d-block" width="100%" height="100%" alt="Portfolio item description">
+                </div>
+                <div class="col-md-12 col-lg-5">
+                  <div class="card-body">
+                    <h5 class="card-title text-center">example.com</h5>
+                    <p class="card-text">This section is for project description.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- /Portfolio Item-->
+
+          <!-- Portfolio Item-->
+          <div class="col-12 col-md-6 col-lg-6 py-3">
+            <div class="card mb-3" style="max-width: 540px;">
+              <div class="row no-gutters">
+                <div class="col-md-12 col-lg-7">
+                  <img src="./partials/img/image-4.png" class="rounded mx-auto d-block" width="100%" height="100%" alt="Portfolio item description">
+                </div>
+                <div class="col-md-12 col-lg-5">
+                  <div class="card-body">
+                    <h5 class="card-title text-center">example.com</h5>
+                    <p class="card-text">This section is for project description.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- /Portfolio Item-->
+
+          <!-- Portfolio Item-->
+          <div class="col-12 col-md-6 col-lg-6 py-3">
+            <div class="card mb-3" style="max-width: 540px;">
+              <div class="row no-gutters">
+                <div class="col-md-12 col-lg-7">
+                  <img src="./partials/img/image-5.png" class="rounded mx-auto d-block" width="100%" height="100%" alt="Portfolio item description">
+                </div>
+                <div class="col-md-12 col-lg-5">
+                  <div class="card-body">
+                    <h5 class="card-title text-center">example.com</h5>
+                    <p class="card-text">This section is for project description.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- /Portfolio Item-->
+
+          <!-- Portfolio Item-->
+          <div class="col-12 col-md-6 col-lg-6 py-3">
+            <div class="card mb-3" style="max-width: 540px;">
+              <div class="row no-gutters">
+                <div class="col-md-12 col-lg-7">
+                  <img src="./partials/img/image-6.png" class="rounded mx-auto d-block" width="100%" height="100%" alt="Portfolio item description">
+                </div>
+                <div class="col-md-12 col-lg-5">
+                  <div class="card-body">
+                    <h5 class="card-title text-center">example.com</h5>
+                    <p class="card-text">This section is for project description.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- /Portfolio Item-->
+
+        </div>
+        <!-- /Portfolio Grid Items-->
+
+        <!-- Version3 Portfolio End here-->
