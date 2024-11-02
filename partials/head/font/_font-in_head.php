62322fc0438b02f8924e318bf3783a99edc00436
Udit Patel <admin@uditpatel.com>
2024-11-02 11:49:07 +0530

    Update _font-in_head.php

--- partials/head/font/_font-in_head.php
+++ partials/head/font/_font-in_head.php
@@ -1,5 +1,1 @@
-<!-- Font Awesome icons Start here -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
-<script src="https://kit.fontawesome.com/1ff0199012.js" crossorigin="anonymous"></script>
-<!-- Font Awesome icons End here -->
