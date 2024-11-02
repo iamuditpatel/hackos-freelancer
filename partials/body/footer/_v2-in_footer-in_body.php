bd4ce29bacbc96e8ce655ed68ecf5f865061b893
Udit Patel <admin@uditpatel.com>
2024-11-02 11:46:16 +0530

    Update _v2-in_footer-in_body.php

--- partials/body/footer/_v2-in_footer-in_body.php
+++ partials/body/footer/_v2-in_footer-in_body.php
@@ -4,28 +4,18 @@
           <!-- Footer Social Icons-->
           <div class="col-lg-4 mb-5 mt-4 mb-lg-0">
             <h4 class="mb-4">THE WEB</h4>
-            <a class="btn btn-outline-light btn-social mx-1" href="#">
-              <i class="fab fa-fw fa-facebook-f"></i>
-            </a>
-            <a class="btn btn-outline-light btn-social mx-1" href="#">
-              <i class="fab fa-fw fa-twitter"></i>
-            </a>
-            <a class="btn btn-outline-light btn-social mx-1" href="#">
-              <i class="fab fa-fw fa-linkedin-in"></i>
-            </a>
-            <a class="btn btn-outline-light btn-social mx-1" href="#">
-              <i class="fab fa-fw fa-instagram"></i>
-            </a>
+            <p class="pre-wrap lead mb-0">Way To Freelancer</p>
+
           </div>
           <!-- /Footer Social Icons-->
 
           <!-- Footer Location-->
           <div class="col-lg-4 mb-5 mt-4 mb-lg-0">
             <h4 class="mb-4">LOCATION</h4>
-            <p class="pre-wrap lead mb-0">912  Patton Lane, North Carolina</p>
+            <p class="pre-wrap lead mb-0">hackOsphere</p>
           </div>
           <!-- /Footer Location-->
-
+          
           <!-- Footer About Text-->
           <div class="col-lg-4 mb-3 mt-4">
             <h4 class="mb-4">ABOUT FREELANCER</h4>
