64068c94e42151689c96c39d990ddbc0c8ad48b4
Udit Patel <admin@uditpatel.com>
2024-11-02 11:44:31 +0530

    Update _v1-in_footer-in_body.php

--- partials/body/footer/_v1-in_footer-in_body.php
+++ partials/body/footer/_v1-in_footer-in_body.php
@@ -1,35 +1,24 @@
 <!-- Version1 Footer Start here-->
 
 <div class="row py-5">
-    <!-- Footer Location-->
-    <div class="col-lg-4 mb-5 mt-4 mb-lg-0">
-        <h4 class="mb-4">LOCATION</h4>
-        <p class="pre-wrap lead mb-0">912  Patton Lane, North Carolina</p>
-    </div>
-    <!-- /Footer Location-->
-    <!-- Footer Social Icons-->
-    <div class="col-lg-4 mb-5 mt-4 mb-lg-0">
-        <h4 class="mb-4">THE WEB</h4>
-        <a class="btn btn-outline-light btn-social mx-1" href="#">
-          <i class="fab fa-fw fa-facebook-f"></i>
-        </a>
-        <a class="btn btn-outline-light btn-social mx-1" href="#">
-          <i class="fab fa-fw fa-twitter"></i>
-        </a>
-        <a class="btn btn-outline-light btn-social mx-1" href="#">
-          <i class="fab fa-fw fa-linkedin-in"></i>
-        </a>
-        <a class="btn btn-outline-light btn-social mx-1" href="#">
-          <i class="fab fa-fw fa-instagram"></i>
-        </a>
-    </div>
-    <!-- /Footer Social Icons-->
-    <!-- Footer About Text-->
-    <div class="col-lg-4 mb-3 mt-4">
-        <h4 class="mb-4">ABOUT FREELANCER</h4>
-        <p class="pre-wrap lead mb-0">Free to use, MIT licensed Bootstrap theme created by Tropos</p>
-    </div>
-    <!-- /Footer About Text-->
-</div>
+              <!-- Footer Location-->
+              <div class="col-lg-4 mb-5 mt-4 mb-lg-0">
+                  <h4 class="mb-4">LOCATION</h4>
+                  <p class="pre-wrap lead mb-0">hackOsphere</p>
+              </div>
+              <!-- /Footer Location-->
+              <!-- Footer Social Icons-->
+              <div class="col-lg-4 mb-5 mt-4 mb-lg-0">
+                  <h4 class="mb-4">THE WEB</h4>
+                  <p class="pre-wrap lead mb-0">Way To Freelancer</p>
+              </div>
+              <!-- /Footer Social Icons-->
+              <!-- Footer About Text-->
+              <div class="col-lg-4 mb-3 mt-4">
+                  <h4 class="mb-4">ABOUT FREELANCER</h4>
+                  <p class="pre-wrap lead mb-0">Free to use, MIT licensed Bootstrap theme created by Tropos</p>
+              </div>
+              <!-- /Footer About Text-->
+          </div>
 
 <!-- /Version1 Footer End here-->
