9c55cbc609c8c3b7aee3552a161d603c03abe459
Udit Patel <admin@uditpatel.com>
2024-11-02 11:47:46 +0530

    Update _v3-in_footer-in_body.php

--- partials/body/footer/_v3-in_footer-in_body.php
+++ partials/body/footer/_v3-in_footer-in_body.php
@@ -9,21 +9,11 @@
 
           <!-- Footer Social Icons-->
           <div class="col-lg-4 mb-5 mt-4 mb-lg-0">
-            <a class="btn btn-outline-light btn-social mx-1" href="#">
-              <i class="fab fa-fw fa-facebook-f"></i>
-            </a>
-            <a class="btn btn-outline-light btn-social mx-1" href="#">
-              <i class="fab fa-fw fa-twitter"></i>
-            </a>
-            <a class="btn btn-outline-light btn-social mx-1" href="#">
-              <i class="fab fa-fw fa-linkedin-in"></i>
-            </a>
-            <a class="btn btn-outline-light btn-social mx-1" href="#">
-              <i class="fab fa-fw fa-instagram"></i>
-            </a>
+            <h4 class="mb-4">THE WEB</h4>
+            <p class="pre-wrap lead mb-0">Way To Freelancer</p>
           </div>
           <!-- /Footer Social Icons-->
-
+  
           <!-- Footer About Text-->
           <div class="col-lg-4 mb-3 mt-4">
             <h4 class="mb-4">ABOUT FREELANCER</h4>
